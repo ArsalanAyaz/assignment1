85bc2f772ae8aec55356b79f20f4d9e7b21c5b8f
ArsalanAyaz <henrykoff7788@gmail.com>
2024-01-12 10:28:13 +0500

    Python class01

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+name='Arsalan Joiya'
+print(name)
+
+Roll_no=208587
+print(Roll_no)
+
+age=24
+print(age)
+
+
+
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
+programing_language="python is my favoruite programming language"
+print(programing_language)
+
+
+
+
